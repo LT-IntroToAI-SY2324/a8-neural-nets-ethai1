ac5a09227985a6b52eb89015167d9c1a7ae233f9
Eric <ethai1@cps.edu>
2024-04-19 11:24:36 -0500

    created network for xor

--- a8.py
+++ a8.py
@@ -2,6 +2,24 @@ from neural import *
 
 print("<<<<<<<<<<<<<< XOR >>>>>>>>>>>>>>\n")
 
+xor_training_data = [
+    ([0, 0], [0]),
+    ([0, 1], [1]),
+    ([1, 0], [1]),
+    ([1, 1], [0])
+]
 
 
+network = NeuralNet(2, 2, 1)
+network.train(xor_training_data)
+print("\n")
+
+network2 = NeuralNet(2, 2, 1)
+network2.train(xor_training_data)
+print("\n")
+
+network3 = NeuralNet(2, 2, 1)
+network3.train(xor_training_data)
+
+# ERROR: 0.36 -> 0.01 -> 0.008
 
