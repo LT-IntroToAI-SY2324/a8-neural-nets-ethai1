720816888d7730c682d49be21376502f15899ac9
Eric <ethai1@cps.edu>
2024-04-22 11:20:05 -0500

    created voter network

--- a8.py
+++ a8.py
@@ -1,25 +1,42 @@
 from neural import *
 
-print("<<<<<<<<<<<<<< XOR >>>>>>>>>>>>>>\n")
+# print("<<<<<<<<<<<<<< XOR >>>>>>>>>>>>>>\n")
 
-xor_training_data = [
-    ([0, 0], [0]),
-    ([0, 1], [1]),
-    ([1, 0], [1]),
-    ([1, 1], [0])
+# xor_training_data = [
+#     ([0, 0], [0]),
+#     ([0, 1], [1]),
+#     ([1, 0], [1]),
+#     ([1, 1], [0])
+# ]
+
+# network = NeuralNet(2, 2, 1)
+# network.train(xor_training_data)
+# print("\n")
+
+# network2 = NeuralNet(2, 8, 1)
+# network2.train(xor_training_data)
+# print("\n")
+
+# network3 = NeuralNet(2, 1, 1)
+# network3.train(xor_training_data)
+
+print("<<<<<<<<<<<<<< VOTERS >>>>>>>>>>>>>>\n")
+
+voting_training_data = [
+    ([0.9, 0.6,	0.8, 0.3, 0.1], [1.0]),
+    ([0.8, 0.8, 0.4, 0.6, 0.4], [1.0]),
+    ([0.7, 0.2,	0.4, 0.6, 0.3],	[1.0]),
+    ([0.5, 0.5,	0.8, 0.4, 0.8],	[0.0]),
+    ([0.3, 0.1,	0.6, 0.8, 0.8],	[0.0]),
+    ([0.6, 0.3,	0.4, 0.3, 0.6],	[0.0])
 ]
 
+voting_network = NeuralNet(5, 8, 1)
+voting_network.train(voting_training_data)
 
-network = NeuralNet(2, 2, 1)
-network.train(xor_training_data)
-print("\n")
-
-network2 = NeuralNet(2, 2, 1)
-network2.train(xor_training_data)
-print("\n")
-
-network3 = NeuralNet(2, 2, 1)
-network3.train(xor_training_data)
-
-# ERROR: 0.36 -> 0.01 -> 0.008
+print(voting_network.evaluate([1.0, 1.0, 1.0, 0.1, 0.1])) # 1
+print(voting_network.evaluate([0.5, 0.2, 0.1, 0.7, 0.7])) # 0
+print(voting_network.evaluate([0.8, 0.3, 0.3, 0.3, 0.8])) # 0
+print(voting_network.evaluate([0.8, 0.3, 0.3, 0.8, 0.3])) # 1
+print(voting_network.evaluate([0.9, 0.8, 0.8, 0.3, 0.6])) # 0 A BIT UNCERTAIN
 
